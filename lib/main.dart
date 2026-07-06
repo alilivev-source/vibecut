53281a4bad71bc70b7dc2b44e3a36543c418af3e
alilivev-source <ali.live.v@gmail.com>
2026-07-07 02:02:44 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'core/localization/app_localizations.dart';
 import 'presentation/controllers/locale_cubit.dart';
 import 'presentation/screens/home_screen.dart';
@@ -7,29 +8,31 @@ import 'presentation/screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppLocalizations.load();
+  await MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => LocaleCubit()..loadSaved(),
       child: BlocBuilder<LocaleCubit, String>(
-        builder: (context, lang) {
-          return MaterialApp(
-            title: 'Video Editor',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.deepPurple, useMaterial3: true),
-            builder: (context, child) => Directionality(
-              textDirection: lang == 'ar' ? TextDirection.rtl : TextDirection.ltr,
-              child: child!,
-            ),
-            home: const HomeScreen(),
-          );
-        },
+        builder: (context, lang) => MaterialApp(
+          title: 'Video Editor',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            colorSchemeSeed: const Color(0xFF6A11CB),
+            useMaterial3: true,
+          ),
+          builder: (context, child) => Directionality(
+            textDirection: lang == 'ar' ? TextDirection.rtl : TextDirection.ltr,
+            child: child!,
+          ),
+          home: const HomeScreen(),
+        ),
       ),
     );
   }
