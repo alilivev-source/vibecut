f6e753e1692ad0dfb12672fc103cfc3cc174d513
alilivev-source <ali.live.v@gmail.com>
2026-07-07 17:38:00 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'core/localization/app_localizations.dart';
 import 'presentation/controllers/locale_cubit.dart';
 import 'presentation/screens/home_screen.dart';
@@ -8,7 +7,6 @@ import 'presentation/screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppLocalizations.load();
-  await MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
